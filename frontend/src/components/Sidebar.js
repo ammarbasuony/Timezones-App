cdea84a7fc1de4c27dbf5be2aad2004cf21cc893
Ammar Yaser <ammaryaser.dev@gmail.com>
2022-06-27 22:33:38 +0200

    [Frontend] Add Logout Button

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import cookies from "js-cookie";
 
 // Assets
 import Logo from "../assets/icons/logo.svg";
@@ -9,6 +10,7 @@ import {
   dashboardIcon,
   barsIcon,
   closeIcon,
+  logoutIcon,
 } from "../helpers/icons";
 
 const Sidebar = () => {
@@ -16,7 +18,7 @@ const Sidebar = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
-  const [pages, setPages] = useState([
+  const pages = [
     {
       name: "Dashboard",
       icon: dashboardIcon("", 17, 17),
@@ -32,7 +34,19 @@ const Sidebar = () => {
       icon: clockIcon("", 17, 17),
       path: "/timezones",
     },
-  ]);
+  ];
+
+  const changePage = (e, path) => {
+    e.preventDefault();
+    setIsOpen(false);
+    navigate(path);
+  };
+
+  const logout = (e) => {
+    e.preventDefault();
+    cookies.remove("timezones_app_token");
+    navigate("/login");
+  };
 
   return (
     <div
@@ -41,7 +55,9 @@ const Sidebar = () => {
       }`}
     >
       <button
-        className={`lg:hidden block absolute transition-all ${isOpen ? "right-3" : "-right-24"} top-3 px-4 py-2 text-white bg-blue-500 rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-blue-200`}
+        className={`lg:hidden block absolute transition-all ${
+          isOpen ? "right-3" : "-right-24"
+        } top-3 px-4 py-2 text-white bg-blue-500 rounded-full shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:ring-offset-blue-200`}
         onClick={() => setIsOpen(!isOpen)}
       >
         {isOpen ? closeIcon("", 23, 23) : barsIcon("", 23, 23)}
@@ -61,16 +77,20 @@ const Sidebar = () => {
                     : "text-gray-500 dark:text-gray-200 hover:text-blue-500"
                 } w-full font-thin uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start`}
                 href={page.path}
-                onClick={(e) => {
-                  e.preventDefault();
-                  setIsOpen(false);
-                  navigate(page.path);
-                }}
+                onClick={(e) => changePage(e, page.path)}
               >
                 <span className="text-left">{page.icon}</span>
                 <span className="mx-4 text-sm font-normal">{page.name}</span>
               </a>
             ))}
+            <a
+              href="/"
+              className="text-gray-500 dark:text-gray-200 hover:text-blue-500 w-full font-thin uppercase flex items-center p-4 my-2 transition-colors duration-200 justify-start"
+              onClick={(e) => logout(e)}
+            >
+              <span className="text-left">{logoutIcon()}</span>
+              <span className="mx-4 text-sm font-normal">Logout</span>
+            </a>
           </div>
         </nav>
       </div>
